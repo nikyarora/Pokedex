9c4e8f5399d0cc0fc26b5907d759858ac33d4165
nikyarora <nikyarora@gmail.com>
2018-02-15 12:29:14 -0800

    create table view cell and collection view cell classes

---
+++ Pokedex/Pokedex/CollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  CollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Niky Arora on 2/15/18.
+//  Copyright © 2018 trainingprogram. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    var pokemonImage: UIImageView!
+    var pokemonCell: UIButton!
+    var pokemonName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        pokemonImage = UIImageView(frame: contentView.frame)
+        pokemonImage.contentMode = .scaleAspectFill
+        pokemonImage.clipsToBounds = true
+        contentView.addSubview(pokemonImage)
+        
+        pokemonName = UILabel(frame: CGRect(x: 0, y: contentView.frame.height * 0.8, width: contentView.frame.width, height: contentView.frame.height * 0.2))
+         pokemonName.clipsToBounds = true
+        pokemonName.textColor = .black
+        pokemonName.backgroundColor = .white
+        pokemonName.font = UIFont(name: "Pokemon Classic", size: 8.0)
+        
+        contentView.addSubview(pokemonName)
+        
+        pokemonCell = UIButton(frame: CGRect(x: 0, y: 0, width: contentView.frame.width-50, height: contentView.frame.height-10))
+        pokemonCell.backgroundColor = .white
+        pokemonCell.sizeToFit()
+        contentView.addSubview(pokemonCell)
+        
+        
+    }
+}
---
+++ Pokedex/Pokedex/ListTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ListTableViewCell.swift
+//  Pokedex
+//
+//  Created by Niky Arora on 2/15/18.
+//  Copyright © 2018 trainingprogram. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    var pokemonImageList: UIImageView!
+    var pokemonNameList: UILabel!
+    var pokemonNumber: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        pokemonImageList = UIImageView(frame: CGRect(x: 20, y: 10, width: 30, height: 30))
+        pokemonNameList = UILabel(frame: CGRect(x: 30, y: 0, width: 40, height: 40))
+        pokemonNameList.textColor = .black
+        pokemonNameList.font = UIFont(name: "Pokemon Classic", size: 9.0)
+        pokemonNumber = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        pokemonNumber.textColor = .black
+        pokemonNumber.font = UIFont(name: "Pokemon Classic", size: 9.0)
+        contentView.addSubview(pokemonImageList)
+        contentView.addSubview(pokemonNameList)
+        contentView.addSubview(pokemonNumber)
+    }
+
+}
